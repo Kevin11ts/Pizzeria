769cd7b3913053d73771a702e94217ee58ab0afa
Kevin11ts <20233l001135@utcv.edu.mx>
2025-02-17 22:45:14 -0600

    Implementar validación en tres casas

--- src/app/hafflepuff/hafflepuff.page.ts
+++ src/app/hafflepuff/hafflepuff.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pedido, PizzeriaService } from '../pizzeria.service';
 
 @Component({
   selector: 'app-hafflepuff',
@@ -7,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class HafflepuffPage implements OnInit {
+ pizzaSeleccionada: string = '';
+  pedidos: Pedido[] = [];
 
-  constructor() { }
+  constructor(private pizzeria: PizzeriaService) { }
 
   ngOnInit() {
   }
 
+  ionViewWillEnter() {
+    this.pedidos = this.pizzeria.getPedidos('Hafflepuff');
+  }
+
+  generarPedido(casa: string, pizza: string): void {
+    if (pizza) {
+      this.pizzeria.generarPedido({ casa, pizza });
+      this.pedidos = this.pizzeria.getPedidos('Hafflepuff'); // Filtra por casa
+    }
+  }
 }
